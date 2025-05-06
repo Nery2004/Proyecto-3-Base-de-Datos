24bab60b068f336f3f3aeec8af2f3318064bcc04
Jose Galindo (Home) <kevin@softlogic.com.gt>
2025-05-06 00:02:31 -0600

    relationships in models

--- app/Models/Autor.php
+++ app/Models/Autor.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Autor extends Model
 {
-    //
+    protected $table = 'autores';
+
+    public function usuario()
+    {
+        return $this->belongsTo(Usuario::class, 'usuario_id');
+    }
 }
