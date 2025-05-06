9afd278afe815e881134deba6ec57fcc7c33e8ce
Jose Galindo (Home) <kevin@softlogic.com.gt>
2025-05-06 00:02:56 -0600

    bootstrap installed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,19 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: 'localhost',
+            protocol: 'ws',
+        },
+        /* watch: {
+            usePolling: true,
+            interval: 100,
+        }, */
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
